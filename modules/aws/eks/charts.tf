7f14996ffd61e5121a86da5b42558666a741db68
Titus <dev@tituscoleman.com>
2025-07-06 12:51:22 -0700

    add: cert-manager

--- modules/aws/eks/charts.tf
+++ modules/aws/eks/charts.tf
@@ -8,4 +8,23 @@ resource "helm_release" "secrets_csi" {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
     value = aws_iam_role.secrets_csi_irsa.arn
   }
+
+}
+
+resource "helm_release" "cert-manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  values = [
+    yamlencode({
+      crds = {
+        enabled = true
+      }
+      #Increase to 2+ in prod
+      replicaCount = 1
+    })
+  ]
 }
